ad5aff342b713d75cbe127d75aaab2c78b5c5544
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-07-30 08:13:02 -0400

    added config files for jest and typescript

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ["ts", "js", "jsx", "tsx"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
+  testEnvironment: "node",
+};
