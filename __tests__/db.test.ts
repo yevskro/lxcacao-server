b57f0f937bf0a98ff629454fd667b71a7d59700e
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-07-30 08:41:11 -0400

    added single db test for quering

---
+++ __tests__/db.test.ts
@@ -0,0 +1,23 @@
+import { Pool } from "pg";
+
+describe("database tests", () => {
+  const conString: string =
+    "postgres://postgres:postgres@127.0.0.1:5432/testdb";
+
+  let pool: Pool;
+
+  beforeAll(() => {
+    pool = new Pool({
+      connectionString: conString,
+    });
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  it("can query database", async () => {
+    const time = await pool.query("SELECT NOW()");
+    expect(time).not.toBe(undefined);
+  });
+});
