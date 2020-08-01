edc0597fdd01b62d7c599a9865281c94a8167718
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-01 15:21:56 -0400

    added more create tables to create.sql also trying to fix an eslint problem with sql array types

--- src/db/create.sql
+++ src/db/create.sql
@@ -13,3 +13,51 @@ CREATE TABLE users
     create_date TIMESTAMP DEFAULT NOW()
 );
 
+CREATE TABLE users_friends
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    friend_id INTEGER
+);
+
+CREATE TABLE users_blocked
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    blocked_id INTEGER
+);
+
+CREATE TABLE users_requests
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    request_id INTEGER
+);
+
+CREATE TABLE recipes
+(
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    time TEXT,
+    type TEXT,
+    private BOOLEAN,
+    img TEXT,
+    ingredients TEXT
+    [],
+    how_to_prepare TEXT[],
+);
+
+CREATE TABLE users_recipes
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    recipe_id INTEGER
+);
+
+CREATE TABLE users_chat
+(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER,
+    friend_id INTEGER,
+    msgs TEXT[]
+);
