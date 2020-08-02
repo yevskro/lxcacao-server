ca0d42aa87ffd3d2d6901184e964ee356fc8c1b6
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-02 17:59:46 -0400

    test(users_friends): add can create a user and friend

--- __tests__/db/tables/users_friends.test.ts
+++ __tests__/db/tables/users_friends.test.ts
@@ -5,14 +5,24 @@ export default (): void => {
   const conString: string = 'postgres://postgres@127.0.0.1:5432/testdb';
 
   let pool: Pool;
-
+  let query: string;
   beforeAll((): void => {
     pool = new Pool({ connectionString: conString });
   });
 
   afterAll(async (): Promise<void> => pool.end());
 
-  it('can create a users friend', (): void => {
-
+  it('can create a users friend', async (): Promise<void> => {
+    query = `
+    INSERT INTO users_friends (user_id, friend_id)
+      VALUES
+    (1, 2)
+    `;
+    try {
+      await pool.query(query);
+    }
+    catch (err) {
+      expect(err).toBe(undefined);
+    }
   });
 };
