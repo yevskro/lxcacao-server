aeca3941371ea9b7272088c616fd4dd18694cb5d
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-02 18:30:21 -0400

    test(db): added testing of users_blocked table

--- __tests__/db/db.test.ts
+++ __tests__/db/db.test.ts
@@ -1,7 +1,9 @@
 import users from './tables/users.test';
 import users_friends from './tables/users_friends.test';
+import users_blocked from './tables/users_blocked.test';
 
 describe('database test!', (): void => {
   describe('users table', users);
   describe('users_friends table', users_friends);
+  describe('users_blocked table', users_blocked);
 });
