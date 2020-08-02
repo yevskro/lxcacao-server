fb81fdd5f17168b9ba9228e7d6b83caba7988d3c
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-02 18:02:28 -0400

    test(users_friends): importing the test into db.test and invoking it

--- __tests__/db/db.test.ts
+++ __tests__/db/db.test.ts
@@ -1,5 +1,7 @@
 import users from './tables/users.test';
+import users_friends from './tables/users_friends.test';
 
 describe('database test!', (): void => {
   describe('users table', users);
+  describe('users_friends table', users_friends);
 });
