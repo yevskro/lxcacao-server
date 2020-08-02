709dd03db35deca433699602d46276aa530b9023
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-02 14:29:38 -0400

    refactor(eslintrc): got rid of unneeded settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
 module.exports = {
-  extends: 'airbnb-typescript',
-  parser: '@typescript-eslint/parser',
+  extends: 'airbnb',
   env: {
     node: true,
     es6: true,
     jest: true
   },
-  parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
   rules: {
     'implicit-arrow-linebreak': 'off',
     'comma-dangle': 'off',
     indent: 'off',
-    'no-trailing-spaces': 'off'
+    'no-trailing-spaces': 'off',
   },
   plugins: ['@typescript-eslint']
 };
--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  moduleFileExtensions: ['ts', 'js', 'jsx', 'tsx'],
+  moduleFileExtensions: ["ts", "js", "jsx", "tsx"],
   transform: {
-    '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.(ts|tsx)$": "ts-jest",
   },
-  testMatch: ['**/__tests__/**/*.[jt]s?(x)'],
-  testEnvironment: 'node',
-  testPathIgnorePatterns: ['/tables/', '/models/']
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)"],
+  testEnvironment: "node",
+  testPathIgnorePatterns: ["/tables/", "/models/"],
 };
