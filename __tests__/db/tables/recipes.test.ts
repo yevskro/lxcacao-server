8420fa9c0ef493fdd62286dceed7e0c7cba11612
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-03 17:16:52 -0400

    feat(create.sql): added check on from full name to not be except an empty string if from_id is set

--- __tests__/db/tables/recipes.test.ts
+++ __tests__/db/tables/recipes.test.ts
@@ -1,5 +1,16 @@
 import { Pool } from 'pg';
-
+/*  
+    name TEXT NOT NULL CHECK (name <> ''),
+    time TEXT NOT NULL CHECK (name <> ''),
+    type TEXT NOT NULL CHECK (type <> ''),
+    private BOOLEAN NOT NULL,
+    ingredients TEXT[] NOT NULL DEFAULT {''},
+    how_to_prepare TEXT[] NOT NULL DEFAULT {''},
+    from_id INTEGER REFERENCES users(id) DEFAULT 0,
+    from_full_name TEXT DEFAULT '',
+    img_file_name TEXT NOT NULL DEFAULT '',
+    create_date TIMESTAMP DEFAULT NOW() 
+*/
 export default (): void => {
   const conString = 'postgres://postgres@127.0.0.1:5432/testdb';
 
--- src/db/seed.sql
+++ src/db/seed.sql
@@ -1,9 +1,9 @@
 INSERT INTO users
   (gmail, first_name, last_name, login_ip, secure_key)
 VALUES
-  ('admin@gmail.com', 'test', 'test', '127.0.0.1', '0001');
+  ('root@gmail.com', 'test', 'test', '127.0.0.1', '0001');
 
 INSERT INTO users
   (gmail, first_name, last_name, login_ip, secure_key)
 VALUES
-  ('root@gmail.com', 'test', 'test', '127.0.0.1', '0002');
+  ('admin@gmail.com', 'test', 'test', '127.0.0.1', '0002');
