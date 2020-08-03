67fa654972da50d2851c94d275a52cc658af8e39
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-03 16:19:07 -0400

    test(recipes): stubbed out recipes test

---
+++ __tests__/db/tables/recipes.test.ts
@@ -0,0 +1,13 @@
+import { Pool } from 'pg';
+
+export default (): void => {
+  const conString = 'postgres://postgres@127.0.0.1:5432/testdb';
+
+  let pool: Pool;
+  let query: string;
+  beforeAll((): void => {
+    pool = new Pool({ connectionString: conString });
+  });
+
+  afterAll(async (): Promise<void> => pool.end());
+};
