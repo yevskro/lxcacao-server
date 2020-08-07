e0e884977cb6d1fcdb671d546508a81023e731d5
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-07 11:10:21 -0400

    refactor(users_blocks.test): asserting on defined postgress error codes

--- __tests__/db/tables/users_blocks.test.ts
+++ __tests__/db/tables/users_blocks.test.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import { Pool } from 'pg';
+import { PostgresError } from 'pg-error-enum';
 
 export default (): void => {
   const conString = 'postgres://postgres@127.0.0.1:5432/testdb';
@@ -39,7 +40,8 @@ export default (): void => {
     } catch (err) {
       error = err;
     }
-    expect(error).not.toBe(undefined);
+
+    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
   });
 
   it('wont create a block user with an invalid id', async (): Promise<void> => {
@@ -54,7 +56,8 @@ export default (): void => {
     } catch (err) {
       error = err;
     }
-    expect(error).not.toBe(undefined);
+
+    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
   });
 
   it('wont create a record without a block_id', async (): Promise<void> => {
@@ -69,7 +72,8 @@ export default (): void => {
     } catch (err) {
       error = err;
     }
-    expect(error).not.toBe(undefined);
+
+    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
   });
 
   it('wont create a record without a user_id', async (): Promise<void> => {
@@ -84,13 +88,15 @@ export default (): void => {
     } catch (err) {
       error = err;
     }
-    expect(error).not.toBe(undefined);
+
+    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
   });
 
   it('record has a create_date', async (): Promise<void> => {
     query = `
     SELECT create_date FROM users_blocks WHERE users_blocks.id=1;
     `;
+
     let error;
     try {
       await pool.query(query);
