34f3e97aa8b3cf0d5840e3e7faccf996a2a9bea2
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-07 17:06:00 -0400

    docs(query error helper): added description of the function

--- __tests__/db/helpers/queryErrorHelper.ts
+++ __tests__/db/helpers/queryErrorHelper.ts
@@ -5,13 +5,13 @@
   Exporting a helper function for tests that
   that query a postgres database and test
   the error code.
-  The error code is an enum type PostgresError.
-  If there is no error undefined is returned.
 */
 import { Pool } from 'pg';
 import { PostgresError } from 'pg-error-enum';
 
 export default async (pool: Pool, query: string): Promise<PostgresError> => {
+  /* The error code is an enum type PostgresError.
+  If there is no error undefined is returned. */
   let error: PostgresError;
   try {
     await pool.query(query);
