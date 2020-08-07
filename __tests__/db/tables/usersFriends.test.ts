05a2728ce04a3aa70ae3caede987e818a701c12a
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-07 14:26:00 -0400

    refactor(users friends test): using queryErrorHelper

--- __tests__/db/tables/usersFriends.test.ts
+++ __tests__/db/tables/usersFriends.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { Pool } from 'pg';
 import { PostgresError } from 'pg-error-enum';
+import queryErrorHelper from '../helpers/queryErrorHelper';
 
 export default (): void => {
   const conString = 'postgres://postgres@127.0.0.1:5432/testdb';
@@ -17,91 +18,59 @@ export default (): void => {
     query = `
     INSERT INTO users_friends (user_id, friend_id)
       VALUES
-    (1, 2)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
-    expect(error).toBe(undefined);
+    (1, 2);`;
+
+    expect(await queryErrorHelper(pool, query)).toBe(undefined);
   });
 
   it('wont create a user with an invalid id', async (): Promise<void> => {
     query = `
     INSERT INTO users_friends (user_id, friend_id)
       VALUES
-    (4, 2)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (4, 2);`;
 
-    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.FOREIGN_KEY_VIOLATION
+    );
   });
 
   it('wont create a friend with an invalid id', async (): Promise<void> => {
     query = `
     INSERT INTO users_friends (user_id, friend_id)
       VALUES
-    (2, 4)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (2, 4);`;
 
-    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.FOREIGN_KEY_VIOLATION
+    );
   });
 
   it('wont create a record without a friend_id', async (): Promise<void> => {
     query = `
     INSERT INTO users_friends (user_id)
       VALUES
-    (1)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (1);`;
 
-    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.NOT_NULL_VIOLATION
+    );
   });
 
   it('wont create a record without a user_id', async (): Promise<void> => {
     query = `
     INSERT INTO users_friends (friend_id)
       VALUES
-    (1)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (1);`;
 
-    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.NOT_NULL_VIOLATION
+    );
   });
 
   it('record has a create_date', async (): Promise<void> => {
     query = `
-    SELECT create_date FROM users_friends WHERE users_friends.id=1;
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
-    expect(error).toBe(undefined);
+    SELECT create_date FROM users_friends WHERE users_friends.id=1;`;
+
+    expect(await queryErrorHelper(pool, query)).toBe(undefined);
   });
 };
