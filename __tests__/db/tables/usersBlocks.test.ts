be6547315d39de273ec47d462d3735c93def2332
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-07 14:19:35 -0400

    refactor(users block test): using queryErrorHelper

--- __tests__/db/tables/usersBlocks.test.ts
+++ __tests__/db/tables/usersBlocks.test.ts
@@ -1,6 +1,7 @@
 /* eslint-disable camelcase */
 import { Pool } from 'pg';
 import { PostgresError } from 'pg-error-enum';
+import queryErrorHelper from '../helpers/queryErrorHelper';
 
 export default (): void => {
   const conString = 'postgres://postgres@127.0.0.1:5432/testdb';
@@ -17,92 +18,59 @@ export default (): void => {
     query = `
     INSERT INTO users_blocks(user_id, block_id)
       VALUES
-    (1, 2)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
-    expect(error).toBe(undefined);
+    (1, 2);`;
+
+    expect(await queryErrorHelper(pool, query)).toBe(undefined);
   });
 
   it('wont create a user with an invalid id', async (): Promise<void> => {
     query = `
     INSERT INTO users_blocks (user_id, block_id)
       VALUES
-    (4, 2)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (4, 2);`;
 
-    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.FOREIGN_KEY_VIOLATION
+    );
   });
 
   it('wont create a block user with an invalid id', async (): Promise<void> => {
     query = `
     INSERT INTO users_blocks (user_id, block_id)
       VALUES
-    (2, 4)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (2, 4);`;
 
-    expect(error.code).toBe(PostgresError.FOREIGN_KEY_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.FOREIGN_KEY_VIOLATION
+    );
   });
 
   it('wont create a record without a block_id', async (): Promise<void> => {
     query = `
     INSERT INTO users_blocks (user_id)
       VALUES
-    (1)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (1);`;
 
-    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.NOT_NULL_VIOLATION
+    );
   });
 
   it('wont create a record without a user_id', async (): Promise<void> => {
     query = `
     INSERT INTO users_blocks (block_id)
       VALUES
-    (1)
-    `;
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
+    (1);`;
 
-    expect(error.code).toBe(PostgresError.NOT_NULL_VIOLATION);
+    expect(await queryErrorHelper(pool, query)).toBe(
+      PostgresError.NOT_NULL_VIOLATION
+    );
   });
 
   it('record has a create_date', async (): Promise<void> => {
     query = `
-    SELECT create_date FROM users_blocks WHERE users_blocks.id=1;
-    `;
+    SELECT create_date FROM users_blocks WHERE users_blocks.id=1;`;
 
-    let error;
-    try {
-      await pool.query(query);
-    } catch (err) {
-      error = err;
-    }
-    expect(error).toBe(undefined);
+    expect(await queryErrorHelper(pool, query)).toBe(undefined);
   });
 };
