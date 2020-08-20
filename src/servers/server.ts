57434bb434e93c9d45eddb42b75d17400300b49f
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-20 13:51:26 -0400

    feat(server): stub routes

--- src/servers/server.ts
+++ src/servers/server.ts
@@ -1,10 +1,18 @@
 import express from 'express';
-import { worker } from 'cluster';
 
 const app = express();
 
-app.get('/', (_, res) => res.send('hello world. son of a gun'));
+app.get('/user/:id/recipe/:id', (_, res) => res.send('user id recipes id'));
+app.patch('/user/:id/recipe/:id', (_, res) => res.send('edit users recipes'));
+app.delete('/user/:id/recipe/:id', (_, res) => res.send('delete recipe'));
+app.post('/user/:id/recipe', (_, res) => res.send('create new recipe'));
 
-app.listen(3000, () => {
-  console.log('SERVER is it woorking?? f thats good. LISTENING. yes? omg!');
+app.get('/user/:id', (_, res) => res.send('user id'));
+
+app.get('/user', (_, res) => res.send('user'));
+app.patch('/user', (_, res) => res.send('edit user(image)'));
+
+app.listen(80, () => {
+  // eslint-disable-next-line no-console
+  console.log('Server is up and running.\nListening on port 80');
 });
