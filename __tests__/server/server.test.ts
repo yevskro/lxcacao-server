011abf81b679c1c9587eeb0cc7b9ef3f406ad928
Yevgeniy Skroznikov <yevskro@gmail.com>
2020-08-23 09:50:38 -0400

    build(ci): checking if ci works

--- __tests__/server/server.test.ts
+++ __tests__/server/server.test.ts
@@ -4,10 +4,12 @@ import httpServer from '../../src/servers/server';
 describe('get /user', () => {
   it('dfd', () => {
     supertest(httpServer)
-      .get('/user/1/recipes')
+      .get('/user/1/recipes/1')
+      .set('Accept', 'application/json')
       .expect(200)
       .end((err) => {
         if (err) throw err;
       });
+    // .then((res) => expect(res.body.name).toStrictEqual());
   });
 });
